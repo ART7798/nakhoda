db4f1ecefae54b2295aa6c41d508f66fff58e474
khashsyarasghari <khasgharidev@gmail.com>
2024-06-23 17:30:24 +0330

    images added for test

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       fontFamily: {
         dana: ["DanaRegular"],
       },
+      fontSize: {
+        15: "15px",
+        13: "13px",
+        12: "12px",
+      },
+      color: {
+        primary: "#fde900",
+        secondary: "#76d4ec",
+        tertiary: "#331209",
+        darkText: "#252525",
+      },
       screens: {
         xs: "480px",
       },
